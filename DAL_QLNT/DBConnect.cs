9ae6d62ea126169383096a86d0bf8791d9a6c2b6
phatnt149 <phat.nt149@gmail.com>
2025-09-18 16:42:38 +0700

    init project n_layer

---
+++ DAL_QLNT/DBConnect.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+namespace DAL_QLNT
+{
+    public class DBConnect
+    {
+        protected SqlConnection _conn = new SqlConnection("Data Source=TIENPHAT;Initial Catalog=QLNT_FIXED;Integrated Security=True;Encrypt=False");
+    }
+}
